35ff4831768e44138dec91f95b5d8f0a86939e7b
Adityaraj1711 <adityamailbox0@gmail.com>
2020-08-21 14:05:54 +0530

    prefConstant changed to kotlin object file from java class file

---
+++ app/src/main/java/com/example/todonotes/PrefConstant.kt
@@ -0,0 +1,12 @@
+package com.example.todonotes
+
+object PrefConstant {
+    @JvmField
+    var SHARED_PREFERENCE_NAME = "notes_app_pref"
+    @JvmField
+    var IS_LOGGED_IN = "is_logged_in"
+    @JvmField
+    var FULL_NAME = "full_name"
+    @JvmField
+    var USER_NAME = "user_name"
+}
