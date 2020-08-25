8a5cc1b500ab790a1302409a097fbbac11371448
Adityaraj1711 <adityamailbox0@gmail.com>
2020-08-25 23:07:09 +0530

    added Glide and image reading support. also requesting the reading file and camera permissions

--- app/src/main/java/com/example/todonotes/view/AddNotesActivity.kt
+++ app/src/main/java/com/example/todonotes/view/AddNotesActivity.kt
@@ -1,9 +1,13 @@
 package com.example.todonotes.view
 
 import android.accounts.AuthenticatorDescription
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.Image
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,8 +15,12 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.example.todonotes.R
 import kotlinx.android.synthetic.main.activity_add_notes.*
 import org.w3c.dom.Text
@@ -24,6 +32,9 @@ class AddNotesActivity : AppCompatActivity() {
     lateinit var imageViewNotes : ImageView
     lateinit var buttonSubmit : Button
     val REQUEST_CODE_GALLERY = 2
+    val REQUEST_CODE_CAMERA = 1
+    var picturePath = ""
+    var MY_PERMISSION_CODE = 124
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
@@ -36,9 +47,30 @@ class AddNotesActivity : AppCompatActivity() {
         imageViewNotes.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
                 Log.d(TAG, "click Listener")
-                setupDialog()
+                if(checkAndRequestPermission()){
+                    setupDialog()
+                }
+
             }
         })
+    }
+
+    private fun checkAndRequestPermission(): Boolean {
+        val cameraPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        val storagePermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        val listPermissionNeeded = ArrayList<String>()
+
+        if(cameraPermission != PackageManager.PERMISSION_GRANTED){
+            listPermissionNeeded.add(android.Manifest.permission.CAMERA)
+        }
+        if(storagePermission != PackageManager.PERMISSION_GRANTED){
+            listPermissionNeeded.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if(!listPermissionNeeded.isEmpty()){
+            ActivityCompat.requestPermissions(this, listPermissionNeeded.toTypedArray<String>(), MY_PERMISSION_CODE)
+            return false
+        }
+        return true
     }
 
     private fun setupDialog() {
@@ -54,7 +86,6 @@ class AddNotesActivity : AppCompatActivity() {
                 //
             }
         })
-
         textViewGallery.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
                 // two types of intent -> implicit and explicit
@@ -71,5 +102,33 @@ class AddNotesActivity : AppCompatActivity() {
         editTextDescription = findViewById(R.id.textInputEditDescription)
         imageViewNotes = findViewById(R.id.imageViewNotes)
         buttonSubmit = findViewById(R.id.buttonAddNotes)
+    }
+
+    // handle the result here when we get data from gallery startActivity
+    // we will use glide made by bumptech, helps us to use or display image in Image view
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            when (requestCode) {
+                REQUEST_CODE_GALLERY -> {
+                    val selectedImage = data?.data
+                    val filePath = arrayOf(MediaStore.Images.Media.DATA)
+                    // c is cursor, its used to do query in Global level in Android app
+                    val c = contentResolver.query(selectedImage!!, filePath, null, null, null)
+                    c!!.moveToFirst()
+                    val columnIndex = c.getColumnIndex(filePath[0])
+                    picturePath = c.getString(columnIndex)
+                    c.close()
+                    Log.d(TAG, picturePath)
+                    Log.d(TAG, selectedImage.toString())
+                    Log.d(TAG, filePath.toString())
+                    Glide.with(this).load(picturePath).into(imageViewNotes)
+                }
+                REQUEST_CODE_CAMERA -> {
+
+                }
+            }
+        }
     }
 }
